ef0d227b29d0cb979b511bf27e1a5793307afdab
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-05 20:22:08 +0200

    modificaciones

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.proyectorefugio {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.xml.bind;
 
 
     opens org.proyectorefugio to javafx.fxml;
