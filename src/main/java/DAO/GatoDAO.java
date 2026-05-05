13838c208ec32d4620716f8e4cc1a77dc538a724
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-05 20:21:48 +0200

    Creado package DAO

---
+++ src/main/java/DAO/GatoDAO.java
@@ -0,0 +1,7 @@
+package DAO;
+
+public class GatoDAO {
+    //TENGO QUE CREAR UN MÉTODO QUE ME HAGA UNA LISTA DE GATOS
+    //EN AnimalDAO tengo el método findByID() que busca todos los animales por su id
+    // en la tabla gato tengo las id de los gatos por lo que tengo que recorrer la tabla y buscar sus ids
+}
---
+++ src/main/java/DAO/PerroDAO.java
@@ -0,0 +1,8 @@
+package DAO;
+
+public class PerroDAO {
+
+    //TENGO QUE CREAR UN MÉTODO QUE ME HAGA UNA LISTA DE PERROS
+    //EN AnimalDAO tengo el método findByID() que busca todos los animales por su id
+    // en la tabla perro tengo las id de los perros por lo que tengo que recorrer la tabla y buscar sus ids
+}
