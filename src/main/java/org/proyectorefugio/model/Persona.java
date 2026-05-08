ae0b4dc77c004b12bce61be1a0ed7db4ded4e662
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-08 09:12:33 +0200

    añadir get y set

--- src/main/java/org/proyectorefugio/model/Persona.java
+++ src/main/java/org/proyectorefugio/model/Persona.java
@@ -16,4 +16,57 @@ public class Persona {
         this.correo = correo;
         this.direccion = direccion;
     }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellidos;
+    }
 }
