43052d1167e27a96b91ab28300c303d0c0f8779e
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-08 21:51:17 +0200

    get y set

--- src/main/java/org/proyectorefugio/model/Ubicacion.java
+++ src/main/java/org/proyectorefugio/model/Ubicacion.java
@@ -17,7 +17,54 @@ public class Ubicacion {
         this.capacidad = capacidad;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public Ubicaciones getTipo() {
+        return tipo;
+    }
 
+    public void setTipo(Ubicaciones tipo) {
+        this.tipo = tipo;
+    }
+
+    public Time getHoraRecreo() {
+        return horaRecreo;
+    }
+
+    public void setHoraRecreo(Time horaRecreo) {
+        this.horaRecreo = horaRecreo;
+    }
+
+    public int getMinutosRecreo() {
+        return minutosRecreo;
+    }
+
+    public void setMinutosRecreo(int minutosRecreo) {
+        this.minutosRecreo = minutosRecreo;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Ubicacion{" +
+                "id=" + id +
+                ", tipo=" + tipo +
+                ", horaRecreo=" + horaRecreo +
+                ", minutosRecreo=" + minutosRecreo +
+                ", capacidad=" + capacidad +
+                '}';
+    }
 }
