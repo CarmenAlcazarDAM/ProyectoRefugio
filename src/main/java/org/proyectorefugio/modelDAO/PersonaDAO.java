da853a8c5af6b67b46d7f0626e4507786ea0e9c2
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-08 09:13:20 +0200

    métodos para findByDni y findAll

--- src/main/java/org/proyectorefugio/modelDAO/PersonaDAO.java
+++ src/main/java/org/proyectorefugio/modelDAO/PersonaDAO.java
@@ -1,4 +1,70 @@
 package org.proyectorefugio.modelDAO;
 
+import org.proyectorefugio.dataAccess.ConnectionBD;
+import org.proyectorefugio.model.Persona;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonaDAO {
+    /**
+     * --------------------Sentencias SQL--------------------
+     **/
+    private final static String SQL_FIND_BY_DNI = "SELECT * FROM persona WHERE dni = ?";
+    private final static String SQL_FIND_ALL = "SELECT * FROM persona";
+    /**------------------------------------------------------**/
+
+    /////////////////////// FIND ///////////////////////
+    /**
+     * Método que busca y devuelve un objeto Persona según su DNI
+     *
+     * @param dni --> dni pasado por parámetro
+     * @return --> devuelve un objeto Persona
+     */
+    public static Persona findByDni(String dni) {
+        Persona p = null;
+        if (!dni.isEmpty()) {
+            return null;
+        }
+        try (PreparedStatement ps = ConnectionBD.getConnection().prepareStatement(SQL_FIND_BY_DNI)) {
+            ps.setString(1, dni);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                String apellidos = rs.getString("apellidos");
+                String telefono = rs.getString("telefono");
+                String correo = rs.getString("correo");
+                String direccion = rs.getString("direccion");
+
+                p = new Persona(dni, nombre, apellidos, telefono, correo, direccion);
+
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return p;
+    }
+
+    /**
+     * Método que crea una lista con todas las personas de la base de datos
+     * @return --> devuelve una lista con todas personas y sus datos
+     */
+    public static List<Persona> findAll() {
+        List<Persona> listaPersonas = new ArrayList<>();
+        Persona p = null;
+        try (ResultSet rs = ConnectionBD.getConnection().createStatement().executeQuery(SQL_FIND_ALL)) {
+            while (rs.next()) {
+                String dni = rs.getString("dni");
+                p = findByDni(dni);
+                listaPersonas.add(p);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return listaPersonas;
+    }
 }
