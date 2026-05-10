9b58f3bff227b5c31d9f1233872ae62508d6a1ed
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-10 11:14:05 +0200

    refactorizacion PerroDAO

--- src/main/java/org/proyectorefugio/model/Perro.java
+++ src/main/java/org/proyectorefugio/model/Perro.java
@@ -4,11 +4,16 @@ import java.util.Date;
 
 public class Perro extends Animal{
 
+    private int idPerro;
     private double peso;
     private boolean agresivo;
 
-    public Perro(int id,String nombre, String raza, Sexo sexo, String marcasDistintivas, String numeroChip, boolean esterilizado, String historia, String observaciones, Date fechaIngreso, int idUbicacion, double peso, boolean agresivo) {
+    public Perro(int id,String nombre, String raza, Sexo sexo,
+                 String marcasDistintivas, String numeroChip, boolean esterilizado,
+                 String historia, String observaciones, Date fechaIngreso,
+                 int idUbicacion, double peso, boolean agresivo) {
         super(id,nombre, raza, sexo, marcasDistintivas, numeroChip, esterilizado, historia, observaciones, fechaIngreso, idUbicacion);
+
         this.peso = peso;
         this.agresivo = agresivo;
     }
@@ -16,6 +21,14 @@ public class Perro extends Animal{
     public Perro(int id, String nombre, String raza, Sexo sexo){
         super(id,nombre, raza,sexo);
     }
+
+    public Perro(int id, String nombre, String raza, Sexo sexo, double peso, boolean agresivo) {
+        super(id,nombre, raza,sexo);
+
+        this.peso = peso;
+        this.agresivo = agresivo;
+    }
+
 
     public double getPeso() {
         return peso;
--- src/main/java/org/proyectorefugio/modelDAO/PerroDAO.java
+++ src/main/java/org/proyectorefugio/modelDAO/PerroDAO.java
@@ -17,12 +17,12 @@ public class PerroDAO {
      * --------------------Sentencias SQL--------------------
      **/
 
-    private final static String SQL_FIND_ALL = "SELECT a.id, a.nombre, a.raza, a.sexo FROM animal a, perro p WHERE a.id = p.idPerro";
-    private final static String SQL_FIND_ALL_NOT_ADOPTED = "SELECT a.id, a.nombre, a.raza, a.sexo FROM animal a, perro p WHERE a.id = p.idPerro AND adoptado = 0";
+    private final static String SQL_FIND_ALL = "SELECT a.id, a.nombre, a.raza, a.sexo FROM animal a, perro p WHERE a.id = p.idPerro AND adoptado = ?";
+
 
     private final static String SQL_FIND_PERRO = "SELECT peso, agresivo FROM perro WHERE idPerro = ?";
 
-    private final static String SQL_INSERT = "INSERT INTO perro VALUES(?)";
+    private final static String SQL_INSERT = "INSERT INTO perro (idPerro, peso, agresivo) VALUES(?, ?, ?)";
 
     private static String SQL_UPDATE_PESO = "UPDATE perro SET peso = ? WHERE idPerro = ?";
     private static String SQL_UPDATE_AGRESIVO = "UPDATE perro SET agresivo = ? WHERE idPerro = ?";
@@ -31,61 +31,46 @@ public class PerroDAO {
 
     /////////////////////// FIND ///////////////////////
     /**
-     * Método que devuelve una lista con todos los perros de la base de datos
-     *
-     * @return --> lista con todos los perros y sus datos.
+     * Método que busca todos los Perros de la base de datos.
+     * @param isAdopted --> filtra si estamos buscando los que están adoptados o no
+     * @return --> devuelve una lista con todos los Perros encontrados
      */
-    public static List<Perro> findAll() {
+    public static List<Perro> findAll(boolean isAdopted) {
         List<Perro> listaPerros = new ArrayList<>();
         Perro perro = null;
-        try (ResultSet rs = ConnectionBD.getConnection().createStatement().executeQuery(SQL_FIND_ALL)) {
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                Animal datosAnimal = AnimalDAO.findByID(id);
+        try (PreparedStatement ps = ConnectionBD.getConnection().prepareStatement(SQL_FIND_ALL)) {
+            ps.setBoolean(1, isAdopted);
 
-                perro = new Perro(id, datosAnimal.getNombre(), datosAnimal.getRaza(), datosAnimal.getSexo());
-                listaPerros.add(perro);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String nombre = rs.getString("nombre");
+                    String raza = rs.getString("raza");
+                    Sexo sexo = Sexo.valueOf(rs.getString("sexo"));
+
+                    perro = new Perro(id, nombre, raza, sexo);
+
+                    listaPerros.add(perro);
+                }
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
         return listaPerros;
     }
 
     /**
-     * Método que devuelve una lista con todos los perros de la base de datos
-     *
-     * @return --> lista con todos los perros y sus datos.
-     */
-    public static List<Perro> findAllNotAdopted() {
-        List<Perro> listaPerros = new ArrayList<>();
-        Perro perro = null;
-        try (ResultSet rs = ConnectionBD.getConnection().createStatement().executeQuery(SQL_FIND_ALL_NOT_ADOPTED)) {
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                Animal datosAnimal = AnimalDAO.findByID(id);
-
-                perro = new Perro(id, datosAnimal.getNombre(), datosAnimal.getRaza(), datosAnimal.getSexo());
-                listaPerros.add(perro);
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return listaPerros;
-    }
-
-    /**
-     * Método que busca a los perros que tengan un nombre específico y estén en adopción
+     * Método que busca a los perros que tengan un nombre específico
      *
      * @param name --> nombre a buscar, introducido por el usuario
+     * @param isAdopted --> filtra si estamos buscando los que están adoptados o no
      * @return --> devuelve una lista con los perros que tengan el nombre introducido por el usuario
      */
-    public static List<Perro> findByNameNotAdopted(String name) {
+    public static List<Perro> findByName(String name, boolean isAdopted) {
         List<Perro> listaPerros = new ArrayList<>();
 
-        List<Animal> animalesEncontrados = AnimalDAO.findByNameNotAdopted(name);
+        List<Animal> animalesEncontrados = AnimalDAO.findByName(name, isAdopted);
 
         for (Animal a : animalesEncontrados) {
             Perro p = rellenarDatosPerro(a);
@@ -134,36 +119,16 @@ public class PerroDAO {
     }
 
     /**
-     * Método que busca a los perros que tengan un nombre específico y ya han sido adoptados
-     *
-     * @param name --> nombre a buscar, introducido por el usuario
-     * @return --> devuelve una lista con los perros que tengan el nombre introducido por el usuario
-     */
-    public static List<Perro> findByNameAdopted(String name) {
-        List<Perro> listaPerros = new ArrayList<>();
-
-        List<Animal> animalesEncontrados = AnimalDAO.findByNameAdopted(name);
-
-        for (Animal a : animalesEncontrados) {
-            Perro p = rellenarDatosPerro(a);
-
-            if (p != null) {
-                listaPerros.add(p);
-            }
-        }
-        return listaPerros;
-    }
-
-    /**
-     * Método que busca a los perros que tengan una raza específica y no han sido adoptados
+     * Método que busca a los perros que tengan una raza específica
      *
      * @param breed --> raza a buscar, introducido por el usuario
+     * @param isAdopted --> filtra si estamos buscando los que están adoptados o no
      * @return --> devuelve una lista con los perros que tengan esa misma raza
      */
-    public static List<Perro> findByBreedNotAdopted(String breed) {
+    public static List<Perro> findByBreed(String breed, boolean isAdopted) {
         List<Perro> listaPerros = new ArrayList<>();
 
-        List<Animal> animalesEncontrados = AnimalDAO.findByBreedNotAdopted(breed);
+        List<Animal> animalesEncontrados = AnimalDAO.findByBreed(breed, isAdopted);
 
         for (Animal a : animalesEncontrados) {
             Perro p = rellenarDatosPerro(a);
@@ -174,38 +139,17 @@ public class PerroDAO {
         }
         return listaPerros;
     }
-
-    /**
-     * Método que busca a los perros que tengan una raza específica y ya han sido adoptados
-     *
-     * @param breed --> raza a buscar, introducido por el usuario
-     * @return --> devuelve una lista con los perros que tengan esa misma raza
-     */
-    public static List<Perro> findByBreedAdopted(String breed) {
-        List<Perro> listaPerros = new ArrayList<>();
-
-        List<Animal> animalesEncontrados = AnimalDAO.findByBreedAdopted(breed);
-
-        for (Animal a : animalesEncontrados) {
-            Perro p = rellenarDatosPerro(a);
-
-            if (p != null) {
-                listaPerros.add(p);
-            }
-        }
-        return listaPerros;
-    }
-
 
     //////////////////////// ADD ////////////////////
     /**
      * Método que inserta un PERRO en la base de datos dentro de la tabla perro
+     *
      * @param p --> objeto Perro pasado como parámetro
      * @param a --> objeto Animal pasado como parámetro
      * @return --> devuelve true si se ha insertado correctamente, false si no se ha insertado
      */
     public static boolean addPerro(Perro p, Animal a) {
-
+        boolean añadido = false;
         if ((p != null) && (a != null)) {
             try (PreparedStatement ps = ConnectionBD.getConnection().prepareStatement(SQL_INSERT)) {
                 ps.setInt(1, a.getId());
@@ -214,57 +158,58 @@ public class PerroDAO {
 
                 ps.executeUpdate();
 
-                return true;
+                añadido = true;
 
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         }
-        return false;
+        return añadido;
     }
 
     //////////////////////// UPDATE ////////////////////
     /**
      * Método que actualiza la informacion del peso del perro
-     * @param p --> perro al que vamos a actualizar la información
+     *
+     * @param p    --> perro al que vamos a actualizar la información
      * @param peso --> peso del perro pasado por parámetro
      * @return --> devuelve true si se actualiza correctamente, false si no lo hace
      */
     public static boolean updatePeso(Perro p, double peso) {
         boolean updated = false;
-        if ((p != null) && AnimalDAO.findByID(p.getId()) != null){
+        if ((p != null) && AnimalDAO.findByID(p.getId()) != null) {
             try (PreparedStatement ps = ConnectionBD.getConnection().prepareStatement(SQL_UPDATE_PESO)) {
-                ps.setDouble(1,peso);
+                ps.setDouble(1, peso);
                 ps.setInt(2, p.getId());
 
                 int filasAfectadas = ps.executeUpdate();
                 updated = (filasAfectadas > 0);
 
-                updated = true;
-            }catch (SQLException e){
+            } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         }
         return updated;
     }
+
     /**
      * Método que actualiza la informacion sobre la agresividad del perro
-     * @param p --> perro al que vamos a actualizar la información
+     *
+     * @param p        --> perro al que vamos a actualizar la información
      * @param agresivo --> agresividad del perro, true si es agresivo, false si no lo es
      * @return --> devuelve true si se actualiza correctamente, false si no lo hace
      */
     public static boolean updateAgresivo(Perro p, boolean agresivo) {
         boolean updated = false;
-        if ((p != null) && AnimalDAO.findByID(p.getId()) != null){
+        if ((p != null) && AnimalDAO.findByID(p.getId()) != null) {
             try (PreparedStatement ps = ConnectionBD.getConnection().prepareStatement(SQL_UPDATE_AGRESIVO)) {
-                ps.setBoolean(1,agresivo);
+                ps.setBoolean(1, agresivo);
                 ps.setInt(2, p.getId());
 
                 int filasAfectadas = ps.executeUpdate();
                 updated = (filasAfectadas > 0);
 
-                updated = true;
-            }catch (SQLException e){
+            } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         }
