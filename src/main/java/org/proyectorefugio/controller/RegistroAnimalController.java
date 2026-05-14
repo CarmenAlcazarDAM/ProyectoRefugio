0048e3ce04ef6db8c4f8616df09dbee8eee0e238
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-14 22:00:58 +0200

    obtenerInformacionGenericaDelFormulario() se encarga de recoger la informacion introducida en el formulario

--- src/main/java/org/proyectorefugio/controller/RegistroAnimalController.java
+++ src/main/java/org/proyectorefugio/controller/RegistroAnimalController.java
@@ -1,9 +1,15 @@
 package org.proyectorefugio.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
+import org.proyectorefugio.enums.Sexo;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Locale;
 
 public class RegistroAnimalController {
 
@@ -16,6 +22,16 @@ public class RegistroAnimalController {
     public CheckBox checkVariable;
     public Text peso;
     public TextField pesoField;
+    public ComboBox opcionesSexo;
+    public TextField infoRaza;
+    public TextField infoColor;
+    public TextField infoEdad;
+    public TextField infoChip;
+    public DatePicker infoFecha;
+    public CheckBox infoEsterilizado;
+    public TextField infoUbicacion;
+    public TextArea infoObservaciones;
+    public TextArea infoHistoria;
 
 
     @FXML
@@ -25,6 +41,27 @@ public class RegistroAnimalController {
      * o de Gato
      */
     public void initialize() {
+        asignarOpcionesSexo();
+        definirTipoAnimal();
+
+
+    }
+
+    /**
+     * Método que asigna las opciones de sexo al ComboBox
+     */
+    public void asignarOpcionesSexo() {
+        ObservableList<String> opciones = FXCollections.observableArrayList(
+                "Hembra",
+                "Macho"
+        );
+        opcionesSexo.setItems(opciones);
+    }
+
+    /**
+     * Método que recibe que tipo de animal está abriendo el formulario
+     */
+    public void definirTipoAnimal() {
         if ("perro".equals(tipo)) {
             checkVariable.setText("¿Es agresivo?");
             peso.setVisible(true);
@@ -32,6 +69,61 @@ public class RegistroAnimalController {
 
         } else if ("gato".equals(tipo)) {
             checkVariable.setText("¿Leucemia?");
+        }
+    }
+
+    /**
+     * Método que va a recoger toda la información procedente de del formulario
+     */
+    public void obtenerInformacionGenericaDelFormulario() {
+        String nombre = infoNombre.getText();
+        String opSexo = opcionesSexo.getValue().toString();
+        Sexo sexo;
+            if (opSexo.equals("hembra")) {
+                sexo = Sexo.hembra;
+            } else if (opSexo.equals("macho")) {
+                sexo = Sexo.macho;
+            }
+        String raza = infoRaza.getText();
+        String color = infoColor.getText();
+        String edad = infoEdad.getText();
+        String numeroChip = infoChip.getText();
+        LocalDate fechaIngreso = infoFecha.getValue();
+
+        Boolean esterilizado;
+            if(infoEsterilizado.isSelected()){
+                esterilizado = true;
+            }else{
+                esterilizado = false;
+            }
+
+        int idUbicacion;
+        String textoUbi = infoUbicacion.getText();
+        idUbicacion = Integer.parseInt(textoUbi);
+
+        String observaciones = infoObservaciones.getText();
+        String historia = infoHistoria.getText();
+        Boolean variable;
+
+        //Independientemente de la información que pida, si está seleccionado será true.
+        if(checkVariable.isSelected()){
+            variable = true;
+        }else{
+            variable = false;
+        }
+
+        if ("perro".equals(tipo)) {obtenerInformacionPerroDelFormulario();}
+    }
+
+    /**
+     * Método que va a recoger la información específica del peso
+     */
+    public void obtenerInformacionPerroDelFormulario() {
+        Double peso;
+        String pesoTexto = pesoField.getText();
+        if (pesoTexto.isEmpty()) {
+
+            peso = Double.parseDouble(pesoTexto);
         }
     }
 
