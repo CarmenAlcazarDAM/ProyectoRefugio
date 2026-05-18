df10a2d907d99d165f5c192eafb25438b25d3cbd
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-18 19:28:51 +0200

    añadidas validaciones de dni y de teléfono

--- src/main/java/org/proyectorefugio/model/Persona.java
+++ src/main/java/org/proyectorefugio/model/Persona.java
@@ -1,10 +1,11 @@
 package org.proyectorefugio.model;
 
-import jdk.jshell.execution.Util;
+
 import org.proyectorefugio.utils.Utils;
 
 import java.util.Objects;
 
+//todo-> comentarios
 public class Persona {
     private String dni;
     private String nombre;
@@ -14,10 +15,10 @@ public class Persona {
     private String direccion;
 
     public Persona(String dni, String nombre, String apellidos, String telefono, String correo, String direccion) {
-        this.dni = dni;
+        setDni(dni);
         this.nombre = nombre;
         this.apellidos = apellidos;
-        this.telefono = telefono;
+        setTelefono(telefono);
         this.correo = correo;
         this.direccion = direccion;
     }
@@ -28,12 +29,15 @@ public class Persona {
 
     public void setDni(String dni) {
 
-        dni = dni.toUpperCase(); //Para que siempre se guarde con la letra en mayúscula
+        String dniMayuscula = dni.toUpperCase().trim();
+        if (dniMayuscula.length() != 9) {
+            throw new IllegalArgumentException("DNI/NIE no válido: debe tener 9 caracteres");
+        }
 
-       if(!Utils.validarDNI(dni)){
-           throw new IllegalArgumentException("DNI/NIE no válido");
-       }
-        this.dni = dni;
+        if (!Utils.validarDNI(dniMayuscula)) {
+            throw new IllegalArgumentException("DNI/NIE no válido");
+        }
+        this.dni = dniMayuscula;
     }
 
     public String getNombre() {
@@ -57,6 +61,10 @@ public class Persona {
     }
 
     public void setTelefono(String telefono) {
+        if (!Utils.validarTelefono(telefono)) {
+
+            throw new IllegalArgumentException("Número de teléfono inválido");
+        }
         this.telefono = telefono;
     }
 
