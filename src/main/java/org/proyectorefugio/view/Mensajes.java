232649f412044f432790ecd91ecf205ae5a40bf4
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-19 09:27:03 +0200

    Mensajes de alerta

--- src/main/java/org/proyectorefugio/view/Mensajes.java
+++ src/main/java/org/proyectorefugio/view/Mensajes.java
@@ -1,6 +1,9 @@
 package org.proyectorefugio.view;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class Mensajes {
 
@@ -27,10 +30,19 @@ public class Mensajes {
         }
         return "Negativo";
     }
+    //todo-> comentar los métodos
+    //region----------MENSAJES CONFIRMACIONES--------------
+
+    public static void operacionCompletada(String mensaje){
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("Operación completada");
+        alerta.setHeaderText("¡Completado con éxito!");
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
+    }
+    //endregion
 
     //region----------MENSAJES ALERTA ERRORES--------------
-    //todo-> comentar los métodos
-
     public static void aletaObligatoriosCamposVacios(String mensaje){
         Alert alerta = new Alert(Alert.AlertType.ERROR);
         alerta.setTitle("ATENCIÓN");
@@ -47,14 +59,17 @@ public class Mensajes {
         alerta.showAndWait();
     }
 
+    //endregion
+
+    //region----------MENSAJES BÚSQUEDAS--------------
     public static void alertaNoExiste(String mensaje){
         Alert alerta = new Alert(Alert.AlertType.WARNING);
         alerta.setTitle("ATENCIÓN");
         alerta.setHeaderText("Búsqueda no encontrada");
         alerta.setContentText(mensaje);
         alerta.showAndWait();
-    }
 
+    }
     public static void alertaNoSeleccionado(String mensaje){
         Alert alerta = new Alert(Alert.AlertType.WARNING);
         alerta.setTitle("ATENCIÓN");
@@ -62,23 +77,50 @@ public class Mensajes {
         alerta.setContentText(mensaje);
         alerta.showAndWait();
     }
-
-    //endregion
-
-    //region----------MENSAJES CONFIRMACIONES--------------
-
-    public static void operacionCompletada(String mensaje){
-        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
-        alerta.setTitle("Operación completada");
-        alerta.setHeaderText("¡Completado con éxito!");
+    public static void alertaYaExiste(String mensaje){
+        Alert alerta = new Alert(Alert.AlertType.WARNING);
+        alerta.setTitle("ATENCIÓN");
+        alerta.setHeaderText("Elemento duplicado");
         alerta.setContentText(mensaje);
         alerta.showAndWait();
     }
 
     //endregion
 
+    //region----------MENSAJES ACTUALIZACIONES--------------
+    public static void actualizacionCorrecta(String mensaje){
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("ÉXITO");
+        alerta.setHeaderText("Modificación completada");
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
+    }
+    public static void actualizacionIncorrecta(String mensaje){
+        Alert alerta = new Alert(Alert.AlertType.ERROR);
+        alerta.setTitle("Error");
+        alerta.setHeaderText("¡No se ha podido completar!");
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
+    }
+    //endregion
 
-    //region----------MENSAJES BÚSQUEDAS--------------
+    //region----------CONFIRMACIÓN ELIMINAR--------------
+    public static boolean confirmarEliminar(String mensaje){
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("ATENCIÓN");
+        alerta.setHeaderText("¿Deseas eliminar este elemento?");
+        alerta.setContentText(mensaje);
+
+        ButtonType btnEliminar = new ButtonType("Eliminar");
+        ButtonType btnCancelar = new ButtonType("Cancelar");
+
+        alerta.getButtonTypes().setAll(btnEliminar, btnCancelar);
+
+        Optional<ButtonType> resultado = alerta.showAndWait();
+        return resultado.isPresent() && resultado.get() == btnEliminar;
+    }
 
     //endregion
+
+
 }
