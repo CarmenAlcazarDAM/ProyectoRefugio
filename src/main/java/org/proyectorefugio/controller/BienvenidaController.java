817964f6235dc0a7aad2e6db906061aed41e60c7
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-21 22:18:00 +0200

    clase comentada entera

--- src/main/java/org/proyectorefugio/controller/BienvenidaController.java
+++ src/main/java/org/proyectorefugio/controller/BienvenidaController.java
@@ -2,7 +2,7 @@ package org.proyectorefugio.controller;
 
 public class BienvenidaController {
     /**
-     * Esta clase solo existe por tener un bienbenida-view.fxml que contiene la pantalla principal
+     * Esta clase solo existe por tener un bienvenida-view.fxml que contiene la pantalla principal
      * del programa y poder volver a mostrarla cuando pulsemos el botón INICIO en nuestra app
      */
 }
