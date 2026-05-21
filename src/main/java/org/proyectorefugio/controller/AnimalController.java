adf5da8a1d0cc24c5842a0b2ecbb65f06b70f725
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-21 22:17:43 +0200

    clase comentada entera

--- src/main/java/org/proyectorefugio/controller/AnimalController.java
+++ src/main/java/org/proyectorefugio/controller/AnimalController.java
@@ -3,21 +3,34 @@ package org.proyectorefugio.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-import javafx.scene.layout.AnchorPane;
 
+import javafx.scene.layout.Pane;
 import org.proyectorefugio.view.SceneManager;
 
-
+/**
+ * Controlador para la gestión de animales del refugio.
+ * Gestiona la navegación con botones entre los distintos tipos de animales.
+ */
 public class AnimalController {
-    //todo -> faltan comentarios
-    @FXML
-    private AnchorPane panelContenido;
 
     @FXML
+    /** Panel contenedor donde se cargan dinámicamente las vistas de cada animal. */
+    private Pane panelContenido;
+
+    @FXML
+    /**
+     * Maneja la carga del fxml correspondiente a la vista de los perros
+     * @param event --> acción que tiene lugar al pulsar el botón
+     */
     public void seleccionarPerro(ActionEvent event) {
        SceneManager.cargarVista(panelContenido,"/org/proyectorefugio/perro-view.fxml");
     }
+
     @FXML
+    /**
+     * Maneja la carga del fxml correspondiente a la vista de los gatos
+     * @param event --> acción que tiene lugar al pulsar el botón
+     */
     public void seleccionarGato(ActionEvent event) {
         SceneManager.cargarVista(panelContenido,"/org/proyectorefugio/gato-view.fxml");
     }
