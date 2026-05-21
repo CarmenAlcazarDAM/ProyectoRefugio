2d205b24de0b5e6716924238645b4917fa382c04
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-21 22:52:37 +0200

    clase comentada entera

--- src/main/java/org/proyectorefugio/controller/PrincipalController.java
+++ src/main/java/org/proyectorefugio/controller/PrincipalController.java
@@ -3,21 +3,27 @@ package org.proyectorefugio.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import org.proyectorefugio.view.SceneManager;
 
-
+/**
+ * Controlador principal de la aplicación.
+ * Gestiona la navegación entre las distintas secciones cargando
+ * las vistas correspondientes.
+ */
 public class PrincipalController {
     @FXML
     private Pane panelContenido;
 
     @FXML
+    /**
+     * Metodo de inicialización
+     * Se encarga de llevar a cabo las primeras acciones que
+     * aparecerán al cargar el archivo fxml
+     */
     private void initialize(){
         SceneManager.cargarVista(panelContenido,"/org/proyectorefugio/bienvenida-view.fxml");
     }
-
-
 
     @FXML
     /**
@@ -27,6 +33,7 @@ public class PrincipalController {
     public void abrirAnimales(ActionEvent event){
         SceneManager.cargarVista(panelContenido,"/org/proyectorefugio/animales-view.fxml");
     }
+
     @FXML
     /**
      * Metodo que abre la ventana voluntario-view.fxml cuando pulsamos el botón Voluntarios en JavaFX
@@ -35,6 +42,7 @@ public class PrincipalController {
     public void abrirVoluntarios(ActionEvent event) {
         SceneManager.cargarVista(panelContenido,"/org/proyectorefugio/voluntario-view.fxml");
     }
+
     @FXML
     /**
      * Metodo que abre la ventana persona-view.fxml cuando pulsamos el botón Adoptantes en JavaFX
@@ -52,6 +60,7 @@ public class PrincipalController {
     public void abrirUbicaciones(ActionEvent event) {
         SceneManager.cargarVista(panelContenido,"/org/proyectorefugio/ubicacion-view.fxml");
     }
+
     @FXML
     /**
      * Metodo que vuelve a la ventana principal cuando pulsamos el botón INICIO en JavaFX
