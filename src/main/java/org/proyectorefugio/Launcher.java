6a2c2c434f80f9f376b883c0de9e1eb6221dec5a
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-21 23:36:06 +0200

    clase comentada entera

--- src/main/java/org/proyectorefugio/Launcher.java
+++ src/main/java/org/proyectorefugio/Launcher.java
@@ -3,7 +3,15 @@ package org.proyectorefugio;
 import javafx.application.Application;
 import org.proyectorefugio.view.PrincipalApplication;
 
+/**
+ * Punto de entrada de la aplicación.
+ * Delega el arranque en  PrincipalApplication para evitar
+ * conflictos con el módulo de JavaFX al ejecutar desde un JAR.
+ */
 public class Launcher {
+    /**
+     * Metodo principal que lanza la aplicación JavaFX.
+     */
     public static void main(String[] args) {
         Application.launch(PrincipalApplication.class, args);
     }
