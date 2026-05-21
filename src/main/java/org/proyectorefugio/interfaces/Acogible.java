8b52ea1b74cfeafa20a9944c632417e2b8c57a71
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-05-21 23:34:52 +0200

    clase comentada entera

--- src/main/java/org/proyectorefugio/interfaces/Acogible.java
+++ src/main/java/org/proyectorefugio/interfaces/Acogible.java
@@ -1,5 +1,11 @@
 package org.proyectorefugio.interfaces;
 
+/**
+ * Interfaz que deben implementar los animales susceptibles de ser acogidos
+ * temporalmente por una persona fuera del refugio.
+ * Si un Perro es agresivo, no puede estar en regimen de Acogida, solo
+ * en Adopción
+ */
 public interface Acogible {
     boolean puedeSerAcogido();
 }
